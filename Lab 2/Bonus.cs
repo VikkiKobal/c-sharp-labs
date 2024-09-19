6e4b8f0bec2613f495f642abbd172faca4de75d7
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-19 16:40:44 +0300

    Update Bonus.cs

--- Lab 2/Bonus.cs
+++ Lab 2/Bonus.cs
@@ -1,1 +1,12 @@
+using System;
+
+namespace Lab_2
+{
+    public class Bonus
+    {
+        public int WorkerCode { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
 
