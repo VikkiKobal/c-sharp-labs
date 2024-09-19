2844d6115eb9a132df50198cc1cbdf585f07d874
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-19 16:41:27 +0300

    Update Program.cs

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeFactory factory = new EmployeeFactory();
+            List<Employee> employees = factory.GetEmployees();
+            List<Bonus> bonuses = factory.GetBonuses();
+
+            EmployeeStatistics.CalculatePositionCountWithBonus(employees, bonuses);
+            EmployeeStatistics.CalculateWomenWithHighSalary(employees, bonuses);
+            EmployeeStatistics.CalculateShopEmployeeBonusCount(employees, bonuses);
+        }
+    }
+}
 
