f306ff8b655800f676bacdc317b984862db8bd22
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-19 16:40:20 +0300

    Update Employee.cs

--- Lab 2/Employee.cs
+++ Lab 2/Employee.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Lab_2
+{
+    public class Employee
+    {
+        public int Code { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public int ShopNumber { get; set; }
+        public string Position { get; set; }
+        public int Experience { get; set; }
+        public double Salary { get; set; }
+    }
+}
