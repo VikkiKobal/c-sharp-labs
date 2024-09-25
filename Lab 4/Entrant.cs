7f7dcaefd7e7456538a075b98ceff1c3b3d42711
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-09-25 17:50:44 +0300

    Create Entrant.cs

---
+++ Lab 4/Entrant.cs
@@ -0,0 +1,10 @@
+namespace Lab_4
+{
+    public class Entrant
+    {
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public int YearOfGraduation { get; set; }
+        public int EntranceScore { get; set; }
+    }
+}
