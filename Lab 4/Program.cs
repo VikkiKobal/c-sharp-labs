869e291a745c21149d5937a2811725821194ada1
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-10-02 08:19:30 +0300

    Update Program.cs

--- Lab 4/Program.cs
+++ Lab 4/Program.cs
@@ -1,83 +1,38 @@
-using System;
+using Lab_4;
 using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
+using System;
 
-namespace Lab_4
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string filePath = @"D:\.net labs\vstup.json";
+        string filePath = @"D:\.net labs\vstup.json";
 
-            CreateJsonFile(filePath);
-
-            Console.WriteLine("Вміст файлу Vstup.json:");
-            DisplayJsonFile(filePath);
-
-            Console.Write("Введіть прізвище для пошуку: ");
-            string surname = Console.ReadLine();
-            DisplayEntrantBySurname(filePath, surname);
-
-            Console.Write("Введіть рік закінчення школи (X): ");
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введіть мінімальний сумарний бал (Y): ");
-            int minScore = Convert.ToInt32(Console.ReadLine());
-
-            CountEntrantsByYearAndScore(filePath, year, minScore);
-
-            Console.ReadKey();
-        }
-
-        static void CreateJsonFile(string filePath)
-        {
-            var entrants = new List<Entrant>
+        var entrants = new List<Entrant>
             {
                 new Entrant { Surname = "Ivanov", Gender = "Чоловіча", YearOfGraduation = 2020, EntranceScore = 190 },
                 new Entrant { Surname = "Petrenko", Gender = "Жіноча", YearOfGraduation = 2021, EntranceScore = 180 },
                 new Entrant { Surname = "Sydorenko", Gender = "Чоловіча", YearOfGraduation = 2019, EntranceScore = 170 }
             };
 
-            string json = JsonConvert.SerializeObject(entrants, Formatting.Indented);
-            File.WriteAllText(filePath, json);
-        }
+        JsonFileHandler.CreateJsonFile(filePath, entrants);
 
-        static void DisplayJsonFile(string filePath)
-        {
-            string jsonContent = File.ReadAllText(filePath);
-            Console.WriteLine(jsonContent);
-        }
+        Console.WriteLine("Вміст файлу Vstup.json:");
+        string jsonContent = JsonFileHandler.ReadJsonFile(filePath);
+        Console.WriteLine(jsonContent);
 
-        static void DisplayEntrantBySurname(string filePath, string surname)
-        {
-            string jsonContent = File.ReadAllText(filePath);
-            var entrants = JsonConvert.DeserializeObject<List<Entrant>>(jsonContent);
+        var entrantManager = new EntrantManager(entrants);
 
-            var entrant = entrants.Find(e => e.Surname.Equals(surname, StringComparison.OrdinalIgnoreCase));
+        Console.Write("Введіть прізвище для пошуку: ");
+        string surname = Console.ReadLine();
+        entrantManager.DisplayEntrantBySurname(surname);
 
-            if (entrant != null)
-            {
-                Console.WriteLine($"\nІнформація про абітурієнта {surname}:");
-                Console.WriteLine($"Стать: {entrant.Gender}");
-                Console.WriteLine($"Рік закінчення школи: {entrant.YearOfGraduation}");
-                Console.WriteLine($"Сумарний бал на вступних екзаменах: {entrant.EntranceScore}");
-            }
-            else
-            {
-                Console.WriteLine($"Абітурієнта з прізвищем {surname} не знайдено.");
-            }
-        }
+        Console.Write("Введіть рік закінчення школи (X): ");
+        int year = Convert.ToInt32(Console.ReadLine());
 
-        static void CountEntrantsByYearAndScore(string filePath, int year, int minScore)
-        {
-            string jsonContent = File.ReadAllText(filePath);
-            var entrants = JsonConvert.DeserializeObject<List<Entrant>>(jsonContent);
+        Console.Write("Введіть мінімальний сумарний бал (Y): ");
+        int minScore = Convert.ToInt32(Console.ReadLine());
 
-            int count = entrants.FindAll(e => e.YearOfGraduation == year && e.EntranceScore >= minScore).Count;
-
-            Console.WriteLine($"\nКількість абітурієнтів, які закінчили школу в {year} і набрали не менше {minScore} балів: {count}");
-        }
+        entrantManager.CountEntrantsByYearAndScore(year, minScore);
     }
 }
