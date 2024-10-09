43928d50b2fc7ccebc6fa57123c67c0a4748ae3b
Viktoriia Kobal <161163769+VikkiKobal@users.noreply.github.com>
2024-10-09 09:08:40 +0300

    Create Student.cs

---
+++ Lab5/Models/Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5___ADO.NET_.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int SchoolGraduationYear { get; set; }
+        public int EntranceExamScore { get; set; }
+    }
+}
+
